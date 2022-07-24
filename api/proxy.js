850ea7ba67590cfa9cb263d8eb6532f2f9aae30f
ssoul11 <907277905@qq.com>
2022-07-24 22:34:49 +0800

    'proxy'

--- api/proxy.js
+++ api/proxy.js
@@ -5,7 +5,7 @@ module.exports = (req, res) => {
 
     // 代理目标地址
     // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-    if (req.url.startsWith('/backend')) {
+    if (req.url.startsWith('/api')) {
         target = 'http://backend-api-02.newbee.ltd/manage-api/v1'
     }
 
@@ -17,7 +17,7 @@ module.exports = (req, res) => {
         pathRewrite: {
             // 通过路径重写，去除请求路径中的 `/backend`
             // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
-            '^/backend/': '/'
+            '^/api/': '/'
         }
     })(req, res)
 }
