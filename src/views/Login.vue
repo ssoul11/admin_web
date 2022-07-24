3ecdc23430a2db90b73efc6d773ae65f7906a14e
ssoul11 <907277905@qq.com>
2022-07-24 13:25:29 +0800

    'check'

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -121,6 +121,7 @@ const submitForm = (formEl: FormInstance | undefined) => {
   formEl.validate((valid) => {
     if (valid) {
       // /adminUser/login 登录接口路径
+      console.log(111111);
       axios
         .post("/adminUser/login", {
           userName: ruleForm.name || "",
@@ -129,12 +130,11 @@ const submitForm = (formEl: FormInstance | undefined) => {
         .then((res) => {
           // 返回的时候会有一个 token，这个令牌就是我们后续去请求别的接口时要带上的，否则会报错，非管理员。
           // 这里我们将其存储到 localStorage 里面。
-          console.log(res);
 
           localSet("token", res);
           console.log(res);
           // 此处登录完成之后，需要刷新页面
-          // window.location.href = "/";
+          window.location.href = "/";
         });
     } else {
       console.log("error submit!");
