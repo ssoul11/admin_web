35884521272cb55719bfbba855d25c3851a670f5
ssoul11 <907277905@qq.com>
2022-07-24 11:38:58 +0800

    'second'

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
   Components({
     resolvers: [ElementPlusResolver()],
   }),],
+  base: './',
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './'),
